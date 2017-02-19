d1dc94ecbe71b916b9b988a93854855c4e118665
Swiip <matthieu.lux@gmail.com>
2017-02-19 18:14:59 +0100

    bumps npm deps

--- test/app/index/index.pkg.js
+++ test/app/index/index.pkg.js
@@ -5,11 +5,11 @@ const TestUtils = require('fountain-generator').TestUtils;
 let context;
 const pkg = {
   devDependencies: {
-    'del': '^2.0.2',
+    'del': '^2.2.2',
     'gulp': 'gulpjs/gulp#4ed9a4a3275559c73a396eff7e1fde3824951ebb',
     'gulp-hub': 'frankwallis/gulp-hub#d461b9c700df9010d0a8694e4af1fb96d9f38bf4',
-    'gulp-filter': '^4.0.0',
-    'gulp-util': '^3.0.7'
+    'gulp-filter': '^5.0.0',
+    'gulp-util': '^3.0.8'
   },
   scripts: {
     'build': 'gulp',
@@ -34,20 +34,20 @@ test('Configuring package.json with angular1/systemjs/css', t => {
     devDependencies: {
       'gulp-useref': '^3.1.2',
       'lazypipe': '^1.0.1',
-      'gulp-postcss': '^6.0.1',
-      'autoprefixer': '^6.2.3',
-      'gulp-rev': '^6.0.1',
-      'gulp-rev-replace': '^0.4.2',
-      'gulp-sourcemaps': '^2.2.0',
-      'gulp-uglify': '^1.4.2',
+      'gulp-postcss': '^6.3.0',
+      'autoprefixer': '^6.7.3',
+      'gulp-rev': '^7.1.2',
+      'gulp-rev-replace': '^0.4.3',
+      'gulp-sourcemaps': '^2.4.1',
+      'gulp-uglify': '^2.0.1',
       'uglify-save-license': '^0.4.1',
-      'gulp-cssnano': '^2.1.0',
-      'gulp-htmlmin': '^1.3.0',
+      'gulp-cssnano': '^2.1.2',
+      'gulp-htmlmin': '^3.0.0',
       'gulp-rename': '^1.2.2',
       'gulp-angular-filesort': '^1.1.1',
-      'gulp-angular-templatecache': '^1.8.0',
+      'gulp-angular-templatecache': '^2.0.0',
       'gulp-insert': '^0.5.0',
-      'gulp-ng-annotate': '^1.1.0'
+      'gulp-ng-annotate': '^2.0.0'
     }
   });
   TestUtils.call(context, 'configuring.pkg', {framework: 'angular1', modules: 'systemjs', css: 'css'});
@@ -58,9 +58,9 @@ test('Configuring package.json with angular1/webpack/css', t => {
   const expected = _.mergeWith({}, pkg, {
     devDependencies: {
       'gulp-angular-filesort': '^1.1.1',
-      'gulp-htmlmin': '^1.3.0',
+      'gulp-htmlmin': '^3.0.0',
       'gulp-insert': '^0.5.0',
-      'gulp-ng-annotate': '^1.1.0'
+      'gulp-ng-annotate': '^2.0.0'
     }
   });
   TestUtils.call(context, 'configuring.pkg', {framework: 'angular1', modules: 'webpack', css: 'css'});
@@ -69,7 +69,7 @@ test('Configuring package.json with angular1/webpack/css', t => {
 
 test('Configuring package.json with angular2/webpack/scss', t => {
   const expected = _.mergeWith({}, pkg, {
-    devDependencies: {'gulp-sass': '^2.1.1'}
+    devDependencies: {'gulp-sass': '^3.1.0'}
   });
   TestUtils.call(context, 'configuring.pkg', {framework: 'angular2', modules: 'webpack', css: 'scss'});
   t.deepEqual(context.mergeJson['package.json'], expected);
@@ -78,19 +78,19 @@ test('Configuring package.json with angular2/webpack/scss', t => {
 test('Configuring package.json with angular2/systemjs/scss', t => {
   const expected = _.mergeWith({}, pkg, {
     devDependencies: {
-      'gulp-sass': '^2.1.1',
-      'gulp-inline-ng2-template': '^2.0.4',
+      'gulp-sass': '^3.1.0',
+      'gulp-inline-ng2-template': '^4.0.0',
       'gulp-useref': '^3.1.2',
       'lazypipe': '^1.0.1',
-      'gulp-postcss': '^6.0.1',
-      'autoprefixer': '^6.2.3',
-      'gulp-rev': '^6.0.1',
-      'gulp-rev-replace': '^0.4.2',
-      'gulp-sourcemaps': '^2.2.0',
-      'gulp-uglify': '^1.4.2',
+      'gulp-postcss': '^6.3.0',
+      'autoprefixer': '^6.7.3',
+      'gulp-rev': '^7.1.2',
+      'gulp-rev-replace': '^0.4.3',
+      'gulp-sourcemaps': '^2.4.1',
+      'gulp-uglify': '^2.0.1',
       'uglify-save-license': '^0.4.1',
-      'gulp-cssnano': '^2.1.0',
-      'gulp-htmlmin': '^1.3.0',
+      'gulp-cssnano': '^2.1.2',
+      'gulp-htmlmin': '^3.0.0',
       'gulp-rename': '^1.2.2'
     }
   });
@@ -100,7 +100,7 @@ test('Configuring package.json with angular2/systemjs/scss', t => {
 
 test('Configuring package.json with angular2/webpack/less', t => {
   const expected = _.mergeWith({}, pkg, {
-    devDependencies: {'gulp-less': '^3.0.5'}
+    devDependencies: {'gulp-less': '^3.3.0'}
   });
   TestUtils.call(context, 'configuring.pkg', {framework: 'angular2', modules: 'webpack', css: 'less'});
   t.deepEqual(context.mergeJson['package.json'], expected);
@@ -108,7 +108,7 @@ test('Configuring package.json with angular2/webpack/less', t => {
 
 test('Configuring package.json with angular2/webpack/styl', t => {
   const expected = _.mergeWith({}, pkg, {
-    devDependencies: {'gulp-stylus': '^2.4.0'}
+    devDependencies: {'gulp-stylus': '^2.6.0'}
   });
   TestUtils.call(context, 'configuring.pkg', {framework: 'angular2', modules: 'webpack', css: 'styl'});
   t.deepEqual(context.mergeJson['package.json'], expected);
